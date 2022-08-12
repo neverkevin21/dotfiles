af92814c80c0f39d9a6880cfeb6fe8056269f56c
neverkevin21 <neverkevin21@github.com>
2022-08-12 17:28:13 +0800

    update rust

--- nvim/lua/conf/rust.lua
+++ nvim/lua/conf/rust.lua
@@ -1,4 +1,8 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
 require('rust-tools').setup({
+    capabilities = capabilities,
     tools = {
         autoSetHints = true,
         hover_with_actions = true,
@@ -6,7 +10,7 @@ require('rust-tools').setup({
             use_telescope = true
         },
         inlay_hints = {
-            show_parameter_hints = false,
+            show_parameter_hints = true,
             parameter_hints_prefix = "",
             other_hints_prefix = "",
         },
@@ -26,6 +30,13 @@ require('rust-tools').setup({
                 checkOnSave = {
                     command = "clippy"
                 },
+                cargo = {
+                    allFeatures = true
+                },
+                assist = {
+                    importPrefix = "crate",
+                    importEnforceGranularity = true
+                }
             }
         }
     },
