671c8b6c5042c7ac33da3c0f7238568c60ee1a9d
neverkevin21 <neverkevin21@github.com>
2022-09-20 21:44:41 +0800

    fix

--- nvim/lua/conf/lualine.lua
+++ nvim/lua/conf/lualine.lua
@@ -1,40 +1,41 @@
-lualine = require'lualine'
+local lualine = require'lualine'
+
 lualine.setup({
-  options = {
-    icons_enabled = true,
-    theme = "tokyonight",
-    -- component_separators = { left = "|", right = "|" },
-    component_separators = {left = "", right = ""},
-    -- https://github.com/ryanoasis/powerline-extra-symbols
-    section_separators = { left = " ", right = "" },
-    always_divide_middle = true,
-  },
-  extensions = { "nvim-tree", "toggleterm" },
-  sections = {
-    lualine_c = {
-      "filename",
-      {
-        "lsp_progress",
-        spinner_symbols = { " ", " ", " ", " ", " ", " " },
-      },
+    options = {
+        icons_enabled = true,
+        theme = "tokyonight",
+        -- component_separators = { left = "|", right = "|" },
+        component_separators = {left = "", right = ""},
+        -- https://github.com/ryanoasis/powerline-extra-symbols
+        section_separators = { left = " ", right = "" },
+        always_divide_middle = true,
     },
-    lualine_x = {
-      "filesize",
-      {
-        "fileformat",
-        -- symbols = {
-        --   unix = '', -- e712
-        --   dos = '', -- e70f
-        --   mac = '', -- e711
-        -- },
-        symbols = {
-          unix = "LF",
-          dos = "CRLF",
-          mac = "CR",
+    extensions = { "nvim-tree", "toggleterm" },
+    sections = {
+        lualine_c = {
+            "filename",
+            {
+                "lsp_progress",
+                spinner_symbols = { " ", " ", " ", " ", " ", " " },
+            },
         },
-      },
-      "encoding",
-      "filetype",
+        lualine_x = {
+            "filesize",
+            {
+                "fileformat",
+                -- symbols = {
+                --   unix = '', -- e712
+                --   dos = '', -- e70f
+                --   mac = '', -- e711
+                -- },
+                symbols = {
+                    unix = "LF",
+                    dos = "CRLF",
+                    mac = "CR",
+                },
+            },
+            "encoding",
+            "filetype",
+        },
     },
-  },
 })
--- nvim/lua/conf/neotest.lua
+++ nvim/lua/conf/neotest.lua
@@ -1,11 +1,11 @@
 require("neotest").setup({
-  adapters = {
-    require("neotest-go")({
-        experimental = {
-            test_table = true,
-        },
-        args = {"-count=1", "-timeout=60s"},
-        dap = {},
-    }),
-  },
+    adapters = {
+        require("neotest-go")({
+            experimental = {
+                test_table = true,
+            },
+            args = {"-count=1", "-timeout=60s"},
+            dap = {},
+        }),
+    },
 })
