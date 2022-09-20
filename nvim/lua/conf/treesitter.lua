630a90390a4d10416f654578b77bcb073e182c35
neverkevin21 <neverkevin21@github.com>
2022-09-20 21:41:36 +0800

    nvim/lua/conf/treesitter: shiftdown

--- nvim/lua/conf/treesitter.lua
+++ nvim/lua/conf/treesitter.lua
@@ -1,20 +1,22 @@
-require'nvim-treesitter'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "go", "lua", "rust", "python", "yaml", "json", "proto"},
+local treesitter = require("nvim-treesitter")
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+treesitter.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = { "go", "lua", "rust", "python", "yaml", "json", "proto"},
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  highlight = {
-    enable = true,
+    -- List of parsers to ignore installing (for "all")
+    -- ignore_install = { "javascript" },
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+    highlight = {
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
 }
