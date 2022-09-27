1faa744c7d62cfd06d2773935238a30709bb43e8
neverkevin21 <neverkevin21@github.com>
2022-09-27 10:00:19 +0800

    conf.tokyonight: update conf

--- nvim/lua/conf/telescope.lua
+++ nvim/lua/conf/telescope.lua
@@ -3,17 +3,40 @@ local telescope = require("telescope")
 telescope.setup({
     defaults = {
         initial_mode = "insert",
+        selection_strategy = "reset",
+        -- sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                -- prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8,
+            },
+            vertical = {
+                mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+        },
         mappings = {
         },
     },
+    file_sorter = require("telescope.sorters").get_fuzzy_file,
     file_ignore_patterns = {
     },
+    path_display = { "truncate" },
+    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+    -- Developer configurations: Not meant for general override
+    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
     pickers = {
         find_files = {
         }
     },
-    extensions = {
-    },
+    extensions_list = { "themes", "terms" },
 })
 
 telescope.load_extension("ui-select")
