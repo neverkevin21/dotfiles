5ec05d0a0ff71d336f0b2aa9595245ea26965902
neverkevin21 <neverkevin21@github.com>
2022-10-09 17:47:20 +0800

    lua/keymaps: fix luasnip keymaps.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -60,15 +60,5 @@ keymap("n", "<leader>c", ":Telescope command_history<CR>", opts)
 keymap("n", "<C-p>", ":Telescope find_files<CR>", opts)
 
 --luasnip
--- press <Tab> to expand or jump in a snippet. These can also be mapped separately
--- via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
-vim.cmd "imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'"
--- -1 for jumping backwards.
-vim.cmd "inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>"
-
 vim.cmd "snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>"
 vim.cmd "snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>"
-
---For changing choices in choiceNodes (not strictly necessary for a basic setup).
-vim.cmd "imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'"
-vim.cmd "smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'"
