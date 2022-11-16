85df755f67ea7d6a1d451814a1e028fcc4a28409
neverkevin21 <neverkevin21@github.com>
2022-11-16 16:54:11 +0800

    bufferline: add NvimTree options.

--- nvim/lua/conf/bufferline.lua
+++ nvim/lua/conf/bufferline.lua
@@ -1,1 +1,15 @@
-require'bufferline'.setup()
+require'bufferline'.setup({
+    options = {
+        diagnostics = "nvim_lsp",
+        offsets = {{
+            filetype = "NvimTree",
+            text = "File Explorer",
+            -- text = function ()
+            --     return vim.fn.getcwd()
+            -- end,
+            highlight = "Pmenu",
+            text_align = "left",
+            separator = true,
+        }}
+    }
+})
