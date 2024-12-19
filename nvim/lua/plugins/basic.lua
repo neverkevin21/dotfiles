b2711c73cc898843aa30dd22d93d1d609005bc9d
neverkevin21 <neverkevin21@github.com>
2024-12-19 14:14:57 +0800

    update plugins

--- nvim/lua/plugins/basic.lua
+++ nvim/lua/plugins/basic.lua
@@ -1,22 +1,5 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		config = function()
-			vim.cmd([[colorscheme catppuccin-macchiato]])
-		end,
-	},
-	{
-		"folke/tokyonight.nvim",
-	},
-	{
-		"rebelot/kanagawa.nvim",
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-	},
-	{
 		"nvim-tree/nvim-tree.lua",
 		dependencies = "nvim-tree/nvim-web-devicons",
 	},
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			vim.cmd([[colorscheme catppuccin-macchiato]])
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+	},
+	{
+		"rebelot/kanagawa.nvim",
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
+}
