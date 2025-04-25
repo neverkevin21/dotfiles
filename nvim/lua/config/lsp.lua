e20b93c0748dd8af6b896756ffc1c12588092adf
neverkevin21 <neverkevin21@github.com>
2025-04-25 13:47:07 +0800

    udpate python lsp

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -5,74 +5,61 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 require("mason").setup({
-	automatic_installation = true,
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
+    automatic_installation = true,
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+        },
+    },
 })
 
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		-- "jsonls",
-		-- "html",
-		-- "yamlls",
-		-- "taplo",
-		-- "lua_ls",
-		-- "bashls",
-		-- "cssls",
-		-- "vimls",
-		-- "tsserver",
-		-- "bufls",
-		-- "sqlls",
-		"tsserver",
-	},
+    ensure_installed = {
+        "pylsp",
+        "tsserver",
+    },
 })
 
 local servers = {
-	"bufls", "tsserver"
-	-- "jsonls", "html", "yamlls", "taplo",
-	-- "lua_ls", "bashls", "cssls", "vimls",
-	-- "tsserver", "bufls", "sqlls",
+    "lua_ls",
 }
 
 for _, ls in ipairs(servers) do
-	lspconfig[ls].setup({
-		capabilities = capabilities,
-		on_attach = function(client, bufnr)
-			require("lsp_signature").on_attach()
-		end,
-	})
+    lspconfig[ls].setup({
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+            require("lsp_signature").on_attach()
+        end,
+    })
 end
 
 lspconfig.tsserver.setup({
-	capabilities = capabilities,
-	on_attach = function(client, bufnr)
-		require("lsp_signature").on_attach()
-	end,
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        require("lsp_signature").on_attach()
+    end,
     filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
 })
 
 require("lsp_signature").setup({
-	bind = true,
-	handler_opts = {
-		border = "rounded",
-	},
-	floating_window = true,
-	-- toggle_key = "<C-k>",
-	hint_enable = false,
-	hi_parameter = "LspSignatureActiveParameter",
+    bind = true,
+    handler_opts = {
+        border = "rounded",
+    },
+    floating_window = true,
+    -- toggle_key = "<C-k>",
+    hint_enable = false,
+    hi_parameter = "LspSignatureActiveParameter",
 })
 
 -- icon
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	underline = true,
-	-- This sets the spacing and the prefix, obviously.
-	virtual_text = {
-		spacing = 4,
-		prefix = "",
-	},
-})
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+-- 	underline = true,
+-- 	-- This sets the spacing and the prefix, obviously.
+-- 	virtual_text = {
+-- 		spacing = 4,
+-- 		prefix = "",
+-- 	},
+-- })
--- nvim/lua/config/python.lua
+++ nvim/lua/config/python.lua
@@ -4,47 +4,57 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local maxLineLength = 109
+
 -- pip install 'python-lsp-server[all]' python-lsp-black python-lsp-isort
+-- :PylspInstall python-lsp-ruff
 lspconfig.pylsp.setup({
-	capabilities = capabilities,
-	on_attach = function(client, bufnr)
-		require("lsp_signature").on_attach()
-	end,
-	settings = {
-		pylsp = {
-			plugins = {
-				-- formatter options
-				black = {
-					enabled = true,
-					line_length = maxLineLength,
-					cache_config = true,
-					-- preview = true,
-				},
-				yapf = { enabled = false },
-				autopep8 = { enabled = false },
-				-- import sorting
-				isort = { enabled = true },
-				-- linter options
-				flake8 = {
-					enabled = true,
-					maxLineLength = maxLineLength,
-					ignore = {},
-				},
-				pyflakes = {
-					enabled = false,
-				},
-				pycodestyle = { enabled = false },
-				-- type checker
-				pylsp_mypy = { enabled = true },
-				-- auto-completion options
-				jedi_completion = { fuzzy = true },
-				jedi_hover = { enabled = true },
-				jedi_references = { enabled = true },
-				jedi_signature_help = { enabled = true },
-				jedi_symbols = { enabled = true, all_scopes = true },
-			},
-		},
-	},
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        require("lsp_signature").on_attach()
+    end,
+    settings = {
+        pylsp = {
+            plugins = {
+                -- formatter options
+                black = {
+                    enabled = true,
+                    line_length = maxLineLength,
+                    cache_config = true,
+                    -- preview = true,
+                },
+                yapf = { enabled = false },
+                autopep8 = { enabled = false },
+                -- import sorting
+                isort = { enabled = true },
+                -- linter options
+                flake8 = {
+                    enabled = false,
+                    maxLineLength = maxLineLength,
+                    ignore = {},
+                },
+                pyflakes = {
+                    enabled = false,
+                },
+                pycodestyle = { enabled = false },
+                -- type checker
+                pylsp_mypy = { enabled = true },
+                -- auto-completion options
+                jedi_completion = { fuzzy = true },
+                jedi_hover = { enabled = true },
+                jedi_references = { enabled = true },
+                jedi_signature_help = { enabled = true },
+                jedi_symbols = { enabled = true, all_scopes = true },
+                ruff = {
+                    enabled = true,
+                    formatEnabled = true,
+                    lineLength = maxLineLength,
+                    select = { "I", "S", "E", "W", "F", "B", "SIM", "FAST", "PL", "G", "T20", "PTH" },
+                    format = { "I", "S", "E", "W", "F", "B", "SIM", "FAST", "PL", "G", "T20", "PTH" },
+                    ignore = {},
+                },
+            },
+        },
+    },
 })
 
 local lspconfig = require("lspconfig")
